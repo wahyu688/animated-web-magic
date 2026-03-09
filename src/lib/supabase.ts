f9e6609d04544b77e91268ff629a89f02489cb66
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-09 23:41:20 +0700

    delete beberapa file untuk clearing

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Paksa gunakan IP Publik agar SSR tidak mencari ke 127.0.0.1
-const supabaseUrl = 'http://31.97.48.241:8000'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZmF1bHQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTk4NDIyOSwiZXhwIjoxOTMxNTYwMjI5fQ.1111111111111111111111111111111111111111111';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
