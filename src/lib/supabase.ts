8a66fb97ab77140c7cd4626500ed30b1b6077e94
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-09 23:22:29 +0700

    Change supabase file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const isServer = typeof window === 'undefined';
-
-// Gunakan nama service 'rest' (port internal 3000) jika di server
-// Gunakan IP Publik (port 8000) jika di browser
-const supabaseUrl = isServer 
-  ? 'http://supabase-rest:3000' 
-  : 'http://31.97.48.241:8000';
-
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZmF1bHQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTk4NDIyOSwiZXhwIjoxOTMxNTYwMjI5fQ.1111111111111111111111111111111111111111111'; // Paste langsung di sini
+// Paksa gunakan IP Publik agar SSR tidak mencari ke 127.0.0.1
+const supabaseUrl = 'http://31.97.48.241:8000'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZmF1bHQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTk4NDIyOSwiZXhwIjoxOTMxNTYwMjI5fQ.1111111111111111111111111111111111111111111';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
