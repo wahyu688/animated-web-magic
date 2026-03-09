37e384d16da230dec2014ca570d3aa6520274d79
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-09 23:02:01 +0700

    change env and docer file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Ganti URL dengan IP VPS Anda dan PORT 8000
-const supabaseUrl = 'http://31.97.48.241:8000/' 
-
-// Gunakan ANON_KEY yang Anda masukkan di Dokploy tadi
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZmF1bHQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTk4NDIyOSwiZXhwIjoxOTMxNTYwMjI5fQ.1111111111111111111111111111111111111111111'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
