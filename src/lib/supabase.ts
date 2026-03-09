46dd0350725d690f4682c4a4c7901dc294dea6b4
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-09 23:06:25 +0700

    Change env file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// Cek apakah kode sedang berjalan di Server (SSR) atau di Browser
+const isServer = typeof window === 'undefined';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Jika di server (Dokploy), gunakan nama service internal Docker jika dalam 1 network
+// Jika di browser, gunakan IP Publik agar user bisa akses
+const supabaseUrl = isServer 
+  ? 'http://supabase-rest:3000' // Ganti 'supabase-rest' sesuai nama service di compose
+  : process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
