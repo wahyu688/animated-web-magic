b03a0a153c5ddbc42c47d08b524d806a70f39766
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-09 22:49:54 +0700

    add supabase file

---
+++ src/lib/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Ganti URL dengan IP VPS Anda dan PORT 8000
+const supabaseUrl = 'http://31.97.48.241/:8000' 
+
+// Gunakan ANON_KEY yang Anda masukkan di Dokploy tadi
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlZmF1bHQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTk4NDIyOSwiZXhwIjoxOTMxNTYwMjI5fQ.1111111111111111111111111111111111111111111'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
