28137c23822be37fb03a7fc9358f5ab38aa7b424
wahyu688 <wahyubarmawi688@gmail.com>
2026-03-11 01:30:29 +0700

    add dashboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"; // Memanggil halaman login yang sudah kita perbaiki
+import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Routes>
         {/* Rute utama untuk halaman Login */}
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         
         {/* Sementara waktu, jika user membuka web "/", langsung arahkan ke /login */}
         <Route path="/" element={<Navigate to="/login" replace />} />
